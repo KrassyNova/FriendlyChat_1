acf5fb68d931980b07b21bae179108f5061df0e5
Krasimira Yordanova <krasimira_yordanova@epam.com>
2025-08-01 14:28:22 +0300

    fix build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+      provideMessaging(() => getMessaging()),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendly-chat-65a97","appId":"1:213706778515:web:9a7fe06ee580369907ea66","databaseURL":"https://friendly-chat-65a97-default-rtdb.firebaseio.com","storageBucket":"friendly-chat-65a97.firebasestorage.app","apiKey":"AIzaSyDnTbavWvrt63-e-r212LZQWdkcxcqAJGk","authDomain":"friendly-chat-65a97.firebaseapp.com","messagingSenderId":"213706778515","measurementId":"G-1107PFH4T1"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
