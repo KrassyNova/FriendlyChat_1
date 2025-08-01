3cac6610f394c5150917762988f2a830171e599a
Krasimira Yordanova <krasimira_yordanova@epam.com>
2025-08-01 14:46:27 +0300

    fix app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+    apiKey: "AIzaSyDnTbavWvrt63-e-r212LZQWdkcxcqAJGk",
+    authDomain: "friendly-chat-65a97.firebaseapp.com",
+    databaseURL: "https://friendly-chat-65a97-default-rtdb.firebaseio.com",
+    projectId: "friendly-chat-65a97",
+    storageBucket: "friendly-chat-65a97.firebasestorage.app",
+    messagingSenderId: "213706778515",
+    appId: "1:213706778515:web:9a7fe06ee580369907ea66",
+    measurementId: "G-1107PFH4T1"
+  }
 };
